307f62956258ff3f14694c8f934a1f342f629924
Metehan Simsek <metehan.simsek@boer.com.tr>
2024-11-21 22:29:30 +0300

    BKUTS_DLL V1.1
    1-EasyModbus ve IO.Ports kütüphaneleri kuruldu.
    2-PLC servis sınıfı oluşturularak PLC'ye özel methodlar bu sınıfta barındırıldı.
    3-DLL için gönderdiğimiz parametreler veritabanı kontrolünden sonra PLC bağlantı ve komut gönderme işlemleri geliştirildi.

---
+++ BKUTSDLL/Services/PLCService.cs
@@ -0,0 +1,51 @@
+﻿using EasyModbus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKUTS_DLL.Services
+{
+    class PLCService
+    {
+        ModbusClient _modbusClient = new ModbusClient();
+
+        public PLCService(string ipAddress, int port) {
+
+            _modbusClient = new ModbusClient(ipAddress, port);
+        }
+
+        public void Connect()
+        {
+            if (!_modbusClient.Connected)
+            {
+                _modbusClient.Connect();
+                Console.WriteLine("PLC ile bağlantı kuruldu");
+            }
+        }
+
+        public void Disconnect()
+        {
+            if (_modbusClient.Connected)
+            {
+                _modbusClient.Disconnect();
+                Console.WriteLine("PLC bağlantısı kapatıldı.");
+            }
+        }
+
+        public void WriteCoil(int address, bool value)
+        {
+            _modbusClient.WriteSingleCoil(address, value);
+            Console.WriteLine($"Coil adresine yazıldı: Adres={address}, Değer={value}");
+
+
+        }
+
+        public int[] ReadRegisters(int startAddress, int quantity)
+        {
+            return _modbusClient.ReadHoldingRegisters(startAddress, quantity);
+        }
+    }
+}
